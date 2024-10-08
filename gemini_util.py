f0c283cf82fdb2710420576b00f7decb65f9af5d
Sandy-giri <giridharanselvam08@gmail.com>
2024-10-08 13:03:20 +0530

    Add files via upload

---
+++ gemini_util.py
@@ -0,0 +1,38 @@
+import os
+from streamlit import secrets
+
+from PIL import Image
+import google.generativeai as genai
+
+API_KEY = "AIzaSyC9QrT3TJGfOAnauNkg50TuIdLFRo5mL1Q"
+genai.configure(api_key=API_KEY)
+
+
+def load_gemini_pro(model_name: str) -> genai.GenerativeModel:
+    """Returns the Gemini Pro Generative model."""
+    model: genai.GenerativeModel = genai.GenerativeModel(model_name=model_name)
+    return model
+
+
+def img_caption(image: Image.Image) -> str:
+    """Returns the response for image captioning prompt."""
+    model: genai.GenerativeModel = load_gemini_pro("gemini-1.5-flash")
+    caption: str = model.generate_content(
+        ["Write a short caption for this image.", image]).text or ""
+    return caption
+
+
+def text_embedding(input_text: str) -> list[float]:
+    """Returns the response containing text embeddings for the given input."""
+    embedding_model: str = "models/embedding-001"
+    embedding: dict = genai.embed_content(model=embedding_model,
+                                          content=input_text,
+                                          task_type="retrieval_document")
+    return embedding["embedding"]
+
+
+def llm_response(user_prompt: str) -> str:
+    """Returns the response from the Gemini Pro LLM for a given user prompt."""
+    llm_model: genai.GenerativeModel = load_gemini_pro("gemini-pro")
+    result: str = llm_model.generate_content(user_prompt).text
+    return result
---
+++ main.py
@@ -0,0 +1,135 @@
+import os
+from PIL import Image
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+from streamlit_util import (lb, page_header, translate_role)
+from gemini_util import (load_gemini_pro, img_caption,
+                         text_embedding, llm_response)
+
+
+# Setting page configuration
+work_dir: str = os.path.dirname(os.path.abspath(__file__))
+st.set_page_config(
+    page_title="QUESTION & ANSWER Chatot",
+    page_icon="💠",
+    layout="centered"
+)
+
+# Options
+selected: str = ""
+options: list[str] = ["ChatBot", "Image captioning",
+                      "Embed text", "Ask me anything"]
+icons: list[str] = ["chat-dots-fill", "image-fill",
+                    "textarea-t", "patch-question-fill"]
+
+
+# Options sidebar
+with st.sidebar:
+    selected = option_menu(
+        menu_title="QUESTION & ANSWER Chatot",
+        options=options,
+        menu_icon="robot",
+        icons=icons,
+        default_index=0
+    )
+
+
+# ChatBot page
+if selected == options[0]:
+    gemini_response: str = ""
+    model = load_gemini_pro("gemini-pro")
+
+    if "chat_session" not in st.session_state:
+        st.session_state.chat_session = model.start_chat(history=[])
+
+    page_header("✨ Q&A bot", color="lightblue")
+
+    # Displaying the chat history so far ...
+    for message in st.session_state.chat_session.history:
+        with st.chat_message(translate_role(message.role)):
+            st.markdown(message.parts[0].text)
+
+    user_prompt: str | None = st.chat_input("Ask Gemini Pro ...")
+    if user_prompt:
+        st.chat_message("user").markdown(user_prompt)
+
+        _, c2, _ = st.columns((3, 2, 1))
+
+        with c2:
+            with st.spinner('Generating response ...'):
+                gemini_response = st.session_state.chat_session.send_message(
+                    user_prompt).text
+
+        with st.chat_message("assistant"):
+            st.markdown(gemini_response)
+
+
+# Image captioning page
+if selected == options[1]:
+    caption: str = ""
+    page_header("📸 Snap Narrate", color="aqua")
+
+    image_upload = st.file_uploader("Generate a descriptive caption for your image.", type=[
+                                    ".jpg", ".jpeg", ".png"])
+
+    if st.button("Generate Caption"):
+        if image_upload:
+            image: Image.Image = Image.open(image_upload)
+
+            st.divider()
+            col1, col2 = st.columns(2, gap="medium")
+
+            with col1:
+                with st.spinner('Generating response ...'):
+                    caption = img_caption(image)
+
+                st.text("Caption")
+                st.info(caption)
+
+            with col2:
+                image.thumbnail((600, 400))
+
+                st.text("Image")
+                st.image(image)
+
+
+# Text embedding page
+if selected == options[2]:
+    embeddings: list[float] = []
+    page_header("🔡 Text Embedding", color="lightgreen")
+
+    input_text: str = st.text_area(
+        label="Enter the text to generate the embeddings.", placeholder="e.g. Who killed Hannibal?")
+
+    if st.button("Get Embeddings"):
+        _, col, _ = st.columns((2, 2, 2))
+
+        with col:
+            lb()
+            with st.spinner('Generating embeddings ...'):
+                embeddings = text_embedding(input_text)
+
+        st.subheader("Embeddings", divider="green")
+        st.markdown(f"""<code style="max-width: 20rem; color: white;">{
+                    embeddings}</code>""", unsafe_allow_html=True)
+
+
+# QnA page
+if selected == options[-1]:
+    result: str = ""
+    page_header("❓ Ask Me Anything", color="slate")
+
+    input_text: str = st.text_area(
+        label="Enter your question.", placeholder="e.g. Can dogs look up?")
+
+    if st.button("Ask Gemini"):
+        _, col, _ = st.columns((2, 1, 2))
+
+        with col:
+            with st.spinner("Thinking ..."):
+                result = llm_response(input_text)
+
+        st.subheader("Answer", divider="blue")
+        st.markdown(result)
